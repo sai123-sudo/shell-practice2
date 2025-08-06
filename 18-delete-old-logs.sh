d3e75c058ac6762548655075e4c5edd7fb9c7e3c
sai123-sudo <ls0122207@gmail.com>
2025-08-06 09:13:30 +0530

    hello worls started

--- 18-delete-old-logs.sh
+++ 18-delete-old-logs.sh
@@ -5,7 +5,7 @@ R="\e[31m"
 G="\e[32m"
 Y="\e[33m"
 N="\e[0m"
-LOGS_FOLDER="/var/log/shellscriptt-logs"
+LOGS_FOLDER="/var/log/shellscript-logs"
 SCRIPT_NAME=$(echo $0 | cut -d "." -f1)
 LOG_FILE="$LOGS_FOLDER/$SCRIPT_NAME.log"
 SOURCE_DIR=/home/ec2-user/app-logs
@@ -20,7 +20,7 @@ else
     echo "You are running with root access" | tee -a $LOG_FILE
 fi
 
-
+# validate functions takes input as exit status, what command they tried to install
 VALIDATE(){
     if [ $1 -eq 0 ]
     then
@@ -31,14 +31,14 @@ VALIDATE(){
     fi
 }
 
-echo "Script started executing at $(date)"
+echo "Script started executing at $(date)" | tee -a $LOG_FILE
 
 FILES_TO_DELETE=$(find $SOURCE_DIR -name "*.log" -mtime +14)
 
-while IFS=read -r filepath
+while IFS= read -r filepath
 do
-    echo "Deleting file: $filepath
+    echo "Deleting file: $filepath" | tee -a $LOG_FILE
     rm -rf $filepath
 done <<< $FILES_TO_DELETE
 
-echo "Script exec sucessfully"
+echo "Script executed successfully"
